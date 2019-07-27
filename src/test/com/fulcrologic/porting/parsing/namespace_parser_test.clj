23b4bb2759fc6c895c0cc3bba8c6b273a83d9d3b
Tony Kay <tony.kay@gmail.com>
2019-07-26 22:39:55 -0700

    added tests for ns parser

--- src/test/com/fulcrologic/porting/parsing/namespace_parser_test.clj
+++ src/test/com/fulcrologic/porting/parsing/namespace_parser_test.clj
@@ -5,13 +5,24 @@
     [fulcro-spec.core :refer [specification assertions behavior]]))
 
 (specification "namespace parser"
-  (let [forms (input/read-string "(ns sample\n  (:use\n    [clojure.java.ui])\n  (:require\n    #? (:clj [clojure.edn :as edn :refer [f g h]] :cljs [boo :as bah])\n    [clojure.java.io :as io :refer :all]))" :clj)
-        env   (nsp/parse-namespace {} (first forms))
-        {:keys [nsalias->ns raw-sym->fqsym]} env]
-    (assertions
-      "Adds resolution for aliased nses"
-      (nsalias->ns 'io) => 'clojure.java.io
-      (nsalias->ns 'edn) => 'clojure.edn
-      "Adds resolution for global syms"
-      (raw-sym->fqsym 'f) => 'clojure.edn/f
-      (raw-sym->fqsym 'h) => 'clojure.edn/h)))
+  (behavior "CLJC Parsing (clj)"
+    (let [clj-forms (input/read-string "(ns sample\n  (:use\n    [clojure.java.ui])\n  (:require\n    #? (:clj [clojure.edn :as edn :refer [f g h]] :cljs [boo :as bah])\n    [clojure.java.io :as io :refer :all]))"
+                      :clj)
+          env       (nsp/parse-namespace {} (first clj-forms))
+          {:keys [nsalias->ns raw-sym->fqsym]} env]
+      (assertions
+        "Adds resolution for aliased nses"
+        (nsalias->ns 'io) => 'clojure.java.io
+        (nsalias->ns 'edn) => 'clojure.edn
+        "Adds resolution for global syms"
+        (raw-sym->fqsym 'f) => 'clojure.edn/f
+        (raw-sym->fqsym 'h) => 'clojure.edn/h)))
+  (behavior "CLJC Parsing (cljs)"
+    (let [forms (input/read-string "(ns sample\n  (:use\n    [clojure.java.ui])\n  (:require\n    #? (:clj [clojure.edn :as edn :refer [f g h]] :cljs [boo :as bah])\n    [clojure.java.io :as io :refer :all]))"
+                  :cljs)
+          env   (nsp/parse-namespace {} (first forms))
+          {:keys [nsalias->ns raw-sym->fqsym]} env]
+      (assertions
+        "Detects the correct differences"
+        (nsalias->ns 'io) => 'clojure.java.io
+        (nsalias->ns 'bah) => 'boo))))
